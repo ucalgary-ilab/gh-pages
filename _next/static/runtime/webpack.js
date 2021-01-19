d2cc821e55150c15735a937381009f71bb0bd71a
Ryo Suzuki <ryosuzk@gmail.com>
2021-01-18 21:59:30 -0700

    Update

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f0d5f4b6da030ebef57f";
+/******/ 	var hotCurrentHash = "beb0b3797fa956f9da60";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

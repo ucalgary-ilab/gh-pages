c72be1e0040a0936840692d533ef8024225c69fd
Ryo Suzuki <ryosuzk@gmail.com>
2023-03-21 17:52:09 -0600

    Update

--- _next/static/development/pages/next/dist/pages/_error.js
+++ _next/static/development/pages/next/dist/pages/_error.js
@@ -4068,7 +4068,7 @@ module.exports = (__webpack_require__(/*! dll-reference dll_6b27229dcf0f0fec5747
 
 /***/ }),
 
-/***/ 1:
+/***/ 2:
 /*!*************************************************************************************************************************************************************************************************************!*\
   !*** multi next-client-pages-loader?page=%2Fnext%2Fdist%2Fpages%2F_error&absolutePagePath=%2FUsers%2Fryosuzuki%2FDocuments%2Fucalgary-ilab%2Filab-website%2Fnode_modules%2Fnext%2Fdist%2Fpages%2F_error.js ***!
   \*************************************************************************************************************************************************************************************************************/
@@ -4091,5 +4091,5 @@ module.exports = dll_6b27229dcf0f0fec5747;
 
 /***/ })
 
-},[[1,"static/runtime/webpack.js"]]]);
+},[[2,"static/runtime/webpack.js"]]]);
 //# sourceMappingURL=_error.js.map
--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "dff77696d4a879d9d723";
+/******/ 	var hotCurrentHash = "9d3e1a1e213c7e9aedc6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

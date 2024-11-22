d2d0cc766d083e3f92123dd01f2667ebe3aecb9f
Clara Xi <clara.xi@ucalgary.ca>
2024-11-22 15:41:32 -0700

    Update

--- _next/static/runtime/main.js
+++ _next/static/runtime/main.js
@@ -6775,7 +6775,7 @@ function connect(options) {
       error.name = err.name;
       error.stack = err.stack; // __NEXT_DIST_DIR is provided by webpack
 
-      source_map_support_1.rewriteStacktrace(error, "/Users/adityagunturu/Documents/Code/Ilab-Website/ilab-website/.next");
+      source_map_support_1.rewriteStacktrace(error, "/home/clara/repos/ilab-website/.next");
       return error;
     }
   };
--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0363a38aea6394123675";
+/******/ 	var hotCurrentHash = "70ec7de235cf68bbbd45";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
